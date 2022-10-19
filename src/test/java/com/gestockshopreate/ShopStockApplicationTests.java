9927b6b45ed9d3bd62d371863ced3110e7b21db0
DJIKINE22 <djikinemamadou2@gmail.com>
2022-10-19 23:15:47 +0000

    first_commit

---
+++ src/test/java/com/gestockshopreate/ShopStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestockshopreate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
